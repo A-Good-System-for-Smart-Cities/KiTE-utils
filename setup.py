a600f9438c03650d3f1f019ba4a4a4edc6c53970
pingle14 <prachi.ingle@utexas.edu>
2022-08-16 12:26:34 -0500

    #4 remv setuptools from dependencies

--- setup.py
+++ setup.py
@@ -90,7 +90,6 @@ setup(
         "pandas>=1.4.3",
         "pytest>=6.2.5",
         "scikit_learn>=1.1.1",
-        "setuptools>=41.2.0,"
         "tqdm>=4.64.0"
     ],
     python_requires=">=3.8",
